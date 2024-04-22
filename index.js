d540609d638b258b82a5ba0ecc4e526b7ad8a47c
CWG <cwg@live.dk>
2024-04-22 11:20:30 +0200

    testing channels

--- index.js
+++ index.js
@@ -17,6 +17,9 @@ client.login(process.env.DISCORD_TOKEN);
 client.on("messageCreate", async (message) =>{
 	console.log(message);
 	if (!message?.author.bot) {
-        	message.author.send(`Echo ${message.content}`);
-        }
+        message.author.send(`Echo ${message.content}`);
+    }
+
+	const channel = await client.channels.fetch('1231691288114499765');
+	channel.send({content: "Test"});
 });
