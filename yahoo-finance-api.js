c1953d823449f0e248a1864ea35de7c40288678f
CWG <cwg@live.dk>
2024-04-22 19:23:32 +0200

    fix

--- yahoo-finance-api.js
+++ yahoo-finance-api.js
@@ -1,4 +1,4 @@
-export async function getCurrentPrice(companyTag){
+export default async function getCurrentPrice(companyTag){
     const response = await fetch(`https://query1.finance.yahoo.com/v8/finance/chart/${companyTag}?period1=1712788800&period2=1713571200&interval=5m&includePrePost=true&events=div%7Csplit%7Cearn&&lang=en-US&region=US`, {
 method: "GET",
 headers: {
